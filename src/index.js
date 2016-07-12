cd83ba29614dac81bb5eea3ae1d0dc6279541836
Cristiano Santos <cristianossd@gmail.com>
2016-07-12 18:22:40 -0300

    Adding first line of block elements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var stage = 1;
 var scene;
 var renderer;
 var camera;
@@ -40,6 +41,8 @@ function renderScene() {
 }
 
 function init() {
+  var colors = [0xF2DC00, 0x00CADB, 0xCC0000, 0x009C0B, 0x725000];
+
   scene = new THREE.Scene();
   renderer = new THREE.WebGLRenderer();
 
@@ -55,10 +58,24 @@ function init() {
 
   bar.name = 'bar';
   bar.position.x = 0;
-  bar.position.y = 1.7;
+  bar.position.y = 1.8;
   bar.position.z = 0;
 
   scene.add(bar);
+
+  var nextBoxX = -0.9;
+  for (var i=0; i<10; i++) {
+    var color = colors[i % colors.length];
+    var boxGeometry = new THREE.BoxGeometry(0.2, 0.1, 0);
+    var boxMaterial = new THREE.MeshBasicMaterial({color: color});
+    var box = new THREE.Mesh(boxGeometry, boxMaterial);
+
+    box.position.x = nextBoxX;
+    box.position.y = -1.4;
+
+    scene.add(box);
+    nextBoxX += 0.2;
+  }
 
   // axis helper
   var globalAxis = new THREE.AxisHelper(1.0);
